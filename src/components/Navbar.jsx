ee699520969d8e01649f45764410c875016478ad
jenya <jenaefremova@gmail.com>
2023-01-25 15:36:45 +0700

    nav module

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <img className="logo__icon" src="../public/react-icon-small.png" alt="logo" />
+                <h3>ReactFacts</h3>
+            </div>          
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
